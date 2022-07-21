d88e01c3ab7df3eb8791ef3b5c6b903de110d72a
DESKTOP-VDDIGHQ\Iniyan <maheshkumar.murugesan>
2022-07-22 00:24:54 +0200

    backend init

---
+++ idealo/src/main/java/com/app/idealo/IdealoApplication.java
@@ -0,0 +1,13 @@
+package com.app.idealo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealoApplication.class, args);
+	}
+
+}
---
+++ idealo/src/test/java/com/app/idealo/IdealoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.idealo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
