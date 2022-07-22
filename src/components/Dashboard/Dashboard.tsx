db92dbff2d96c45f746db6d4f26243fc274833b0
DESKTOP-VDDIGHQIniyan <maheshkumar.murugesan>
2022-07-23 01:45:39 +0200

    product list

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
+import RecipeReviewCard from '../Card/Card';
 import styles from './Dashboard.module.scss';
 
 interface DashboardProps {}
 
 const Dashboard: FC<DashboardProps> = () => (
   <div className={styles.Dashboard} data-testid="Dashboard">
-    Dashboard Component
+    <RecipeReviewCard/>
   </div>
 );
 
