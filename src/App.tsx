a9925fdb6323c32522eade85ee666d7d481dee0a
DESKTOP-VDDIGHQIniyan <maheshkumar.murugesan>
2022-07-23 20:08:19 +0200

    products

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 import ResponsiveAppBar from './components/ResponsiveAppBar/ResponsiveAppBar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import CreateProduct from './components/CreateProduct/CreateProduct';
+import Card from './components/Card/Card';
 
 function App() {
   return (
-    <div className="App">
-      <ResponsiveAppBar />
-      <div className='App-Start'>
-        <Dashboard />  
-        <Dashboard />  
-        <Dashboard />  
-        <Dashboard />      
-      </div>     
-    </div>
+    <Router>
+      <div className="App">
+        <ResponsiveAppBar />       
+      </div>
+
+      <Routes>
+        <Route  path='/' element={< Dashboard />}></Route>
+        <Route  path='/products' element={< Dashboard />}></Route>
+        <Route  path='/newproduct' element={< CreateProduct />}></Route>
+      </Routes>
+    </Router>
+
   );
 }
 
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,26 @@
-import React, { FC } from 'react';
+import { useEffect, useState } from 'react';
 import RecipeReviewCard from '../Card/Card';
 import styles from './Dashboard.module.scss';
 
-interface DashboardProps {}
+interface DashboardProps { }
 
-const Dashboard: FC<DashboardProps> = () => (
-  <div className={styles.Dashboard} data-testid="Dashboard">
-    <RecipeReviewCard/>
-  </div>
-);
+function Dashboard(DashboardProps: any) {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (<div className={styles.Dashboard} data-testid="Dashboard">
+    {products && products.map((data:any) => {
+      return <RecipeReviewCard product={data} />;
+    })
+    }
+  </div>)
+
+}
 
 export default Dashboard;
