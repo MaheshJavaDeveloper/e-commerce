15dc1caa285f4a1e14f148a0d05aa275fef6b951
DESKTOP-VDDIGHQ\Iniyan <maheshkumar.murugesan>
2022-07-24 19:47:36 +0200

    Junit testcases

--- idealo/src/test/java/com/app/idealo/IdealoApplicationTests.java
+++ idealo/src/test/java/com/app/idealo/IdealoApplicationTests.java
@@ -1,13 +1,67 @@
 package com.app.idealo;
 
+import com.app.idealo.model.Product;
+import com.app.idealo.service.ProductService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class IdealoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testHistoricalBestPriceUpdated()  {
+		Product product = new Product();
+		product.setCurrentPrice(20);
+		product.setHistoricalBestPrice(50);
+
+		ProductService productService=new ProductService();
+		productService.handlePrice(product);
+
+		assertThat(product.getHistoricalBestPrice()).isEqualTo(product.getCurrentPrice());
+
+	}
+
+	@Test
+	void testHistoricalBestPriceandDateUpdated()  {
+		Product product = new Product();
+		product.setCurrentPrice(20);
+		product.setHistoricalBestPrice(50);
+
+		ProductService productService=new ProductService();
+		productService.handlePrice(product);
+
+		assertThat(product.getHistoricalBestPrice()).isEqualTo(product.getCurrentPrice());
+		assertThat(product.getBestPriceDate()).isNotNull();
+
+	}
+
+	@Test
+	void testHistoricalBestPriceNotUpdated()  {
+		Product product = new Product();
+		product.setCurrentPrice(20);
+		product.setHistoricalBestPrice(10);
+
+		ProductService productService=new ProductService();
+		productService.handlePrice(product);
+
+		assertThat(product.getHistoricalBestPrice()).isEqualTo(10);
+
+	}
+
+	@Test
+	void testHistoricalBestPriceDateNotUpdated()  {
+		Product product = new Product();
+		product.setCurrentPrice(20);
+		product.setHistoricalBestPrice(10);
+
+		ProductService productService=new ProductService();
+		productService.handlePrice(product);
+
+		assertThat(product.getHistoricalBestPrice()).isEqualTo(10);
+		assertThat(product.getBestPriceDate()).isNull();
 	}
 
 }
